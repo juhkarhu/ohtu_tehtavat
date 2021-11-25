ffa28c11eba2aa41195590972202019530a03330
Juhana Karhunen <juhana.karhunen@gmail.com>
2021-11-25 10:40:21 +0200

    vko4 teht5 ostoskori tdd-tekniikalla, testi nro 9 tehty

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -41,13 +41,13 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(self.kori.hinta(), 14)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
- 
+        self.kori.lisaa_tuote(self.maito)
         ostokset = self.kori.ostokset()
-
         self.assertEqual(len(ostokset), 1)
 
-
-
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio_jolla_oikea_tuotteen_nimi_ja_maara(self):
+        self.kori.lisaa_tuote(self.maito)
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual(ostos.tuotteen_nimi(), 'Maito')
+        self.assertEqual(ostos.lukumaara(), 1)
 
