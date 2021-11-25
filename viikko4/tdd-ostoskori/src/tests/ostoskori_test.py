72879bd68a1f45ff5b639af2f39a4d840f32fc44
Juhana Karhunen <juhana.karhunen@gmail.com>
2021-11-25 10:31:51 +0200

    vko4 teht5 ostoskori tdd-tekniikalla, testi nro 5 tehty

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,24 +5,27 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.maito = Tuote("Maito", 3)
+        self.kahvi = Tuote('Kahvi', 7)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
         self.assertEqual(self.kori.hinta(), 0)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_sama_kuin_tuotteen_hinta(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.hinta(), 3)
 
-    def test_kahden_eri_tuotteen_lisäämisen_jälkeen_ostoskorissa_on_2_tavaraa(self):
-        maito = Tuote("Maito", 3)
-        kahvi = Tuote('Kahvi', 7)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(kahvi)
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_2_tavaraa(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.kahvi)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+    
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteiden_hintojen_summa(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.kahvi)
+        self.assertEqual(self.kori.hinta(), 10)
