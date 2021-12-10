e52eb7be99ec52657f310d2f0af25d5c9545d437
Juhana Karhunen <juhana.karhunen@gmail.com>
2021-12-10 15:08:57 +0200

    small tweaks

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -11,18 +11,13 @@ def main():
     query = QueryBuilder()
 
     matcher = (
-        query
-            .oneOf(
-            query.playsIn("PHI")
-                .hasAtLeast(10, "assists")
-                .hasFewerThan(5, "goals")
-                .build(),
-            query.playsIn("EDM")
-                .hasAtLeast(40, "points")
-                .build()
-            )
-            .build()
-        )
+      query  
+        .playsIn("NYR")  
+        .hasAtLeast(5, "goals")  
+        .hasFewerThan(10, "goals")  
+        .build()
+    )
+
 
 
     for player in stats.matches(matcher):
--- viikko6/query-language/src/query_builder.py
+++ viikko6/query-language/src/query_builder.py
@@ -1,20 +1,20 @@
 from matchers import And, Or, All, HasAtLeast, HasFewerThan, PlaysIn
 
 class QueryBuilder:
-  def __init__(self, matchers = And()):
-    self.matchers = matchers
+  def __init__(self, matchers=All()):
+      self.matchers = matchers
 
   def build(self):
-    return self.matchers
+      return self.matchers
 
   def playsIn(self, team):
-    return QueryBuilder(PlaysIn(team))
+      return QueryBuilder(PlaysIn(team))
 
   def hasAtLeast(self, value, attr):
-    return QueryBuilder(And(self.matchers, HasAtLeast(value, attr)))
+      return QueryBuilder(And(self.matchers, HasAtLeast(value, attr)))
 
   def hasFewerThan(self, value, attr):
-    return QueryBuilder(And(self.matchers, HasFewerThan(value, attr)))
+      return QueryBuilder(And(self.matchers, HasFewerThan(value, attr)))
 
-  def oneOf(self, matcher1, matcher2):
-    return QueryBuilder(Or(matcher1, matcher2))
+  def oneOf(self, query1, query2):
+      return QueryBuilder(Or(query1, query2))
